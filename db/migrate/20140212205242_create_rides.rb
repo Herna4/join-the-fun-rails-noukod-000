e8731e5920c65546f5f2d6e20dca125653893d4f
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-07 23:01:07 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -4,7 +4,7 @@ class CreateRides < ActiveRecord::Migration
       t.integer :passenger_id
        t.integer :taxi_id
        t.datetime :ride_date
-       
+
        t.timestamps null: false
     end
   end
